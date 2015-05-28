fc744ba27a9faf2f08244c50c8ac0590b025cc40
Mingxuan Wang <miw072@eng.ucsd.edu>
2015-05-27 21:17:12 -0700

    crud

--- hw4/Sundogs/js/additem.js
+++ hw4/Sundogs/js/additem.js
@@ -2,6 +2,35 @@
  * Created by Mingxuan on 2015/5/27.
  */
 $(document).ready(function(){
+
+    var type = $("#new_type").val();
+    var myTypeRef = new Firebase("https://flickering-heat-2946.firebaseio.com/CoinType/Gold/" + type + "");
+
+
+    myTypeRef.on('value', function(snapshot){
+        var curr = snapshot.val();
+
+        var goldp = curr.Goldp;
+        var goldozt = curr.Goldozt;
+        var goldg = curr.Goldg;
+        var weight = curr.Weight;
+        var totalau = goldozt * $("#new_quantity").val();
+
+        $("#new_goldg").empty();
+        $("#new_goldp").empty();
+        $("#new_goldozt").empty();
+        $("#new_weight").empty();
+        $("#new_totalau").empty();
+
+
+        $("#new_goldg").append(goldg);
+        $("#new_goldp").append(goldp);
+        $("#new_goldozt").append(goldozt);
+        $("#new_weight").append(weight);
+        $("#new_totalau").append(totalau);
+
+    });
+
     $("#save").click(function(){
         var myFirebaseRef = new Firebase("https://flickering-heat-2946.firebaseio.com/User/Richard/Gold");
         var metal = $("#new_metal").val();
--- hw4/Sundogs/js/database.js
+++ hw4/Sundogs/js/database.js
@@ -4,6 +4,8 @@
 
 $(document).ready(function(){
 
+
+
     $("#edit_save").click(function(){
 
         var query = window.location.search;
@@ -31,15 +33,51 @@ $(document).ready(function(){
 
     myFirebaseRef.on('value', function(snapshot){
         var curr = snapshot.val();
+        var type = curr.Type;
 
         $("#Metal").append(curr.Metal);
         $("#Type").append(curr.Type);
         $("#Date").append(curr.Date);
-        $("#Goldp").append(curr.Goldp);
-        $("#Weight").append(curr.Weight);
-    });
 
 
+        var myTypeRef = new Firebase("https://flickering-heat-2946.firebaseio.com/CoinType/Gold/" + type + "");
+
+
+        myTypeRef.on('value', function(snapshot){
+            var curr = snapshot.val();
+
+            var goldp = curr.Goldp;
+            var goldozt = curr.Goldozt;
+            var goldg = curr.Goldg;
+            var weight = curr.Weight;
+            var totalau = goldozt * $("#Qty").val();
+
+            $("#Goldg").empty();
+            $("#Goldp").empty();
+            $("#Goldozt").empty();
+            $("#Weight").empty();
+            $("#TotalAu").empty();
+
+
+            $("#Goldg").append(goldg);
+            $("#Goldp").append(goldp);
+            $("#Goldozt").append(goldozt);
+            $("#Weight").append(weight);
+            $("#TotalAu").append(totalau);
+
+
+            $("#Qty").keyup(function(){
+                    var goldozt = curr.Goldozt;
+                    var totalau = goldozt * $("#Qty").val();
+
+                    $("#TotalAu").empty();
+
+                    $("#TotalAu").append(totalau);
+
+                });
+            });
+
+        });
 
 });
 
